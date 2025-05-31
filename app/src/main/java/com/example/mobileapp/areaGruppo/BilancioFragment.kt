6d2d690d22b55b6152059a12580c54fae539f643
Festino2000 <festa6941@gmail.com>
2025-05-31 20:23:28 +0200

    Fix Visibilita Tema Scuro

--- app/src/main/java/com/example/mobileapp/areaGruppo/BilancioFragment.kt
+++ app/src/main/java/com/example/mobileapp/areaGruppo/BilancioFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.example.mobileapp.R
 
@@ -13,5 +14,6 @@ class BilancioFragment : Fragment(R.layout.fragment_bilancio) {
     ): View = TextView(requireContext()).apply {
         text = "Bilancio"
         textSize = 24f
+        setTextColor(ResourcesCompat.getColor(resources, R.color.bilancio_text_color, null))
     }
 }

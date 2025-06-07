585493122b135f2dc60556855677943a93a0ffe4
Festino2000 <festa6941@gmail.com>
2025-06-07 03:25:07 +0200

    Aggiunta Logout

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileapp.areaGruppo.GruppoActivity
 import com.example.mobileapp.areaPersonale.SoloActivity
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import androidx.appcompat.app.AlertDialog
+import com.example.mobileapp.gestioneAccesso.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         val soloButton = findViewById<ImageButton>(R.id.solo)
         val gruppoButton = findViewById<ImageButton>(R.id.gruppo)
+        val logoutButton = findViewById<ImageButton>(R.id.logoutButton)
+
+        logoutButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Conferma Logout")
+                .setMessage("Sei sicuro di voler uscire?")
+                .setPositiveButton("Sì") { _, _ ->
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                .setNegativeButton("Annulla", null)
+                .show()
+        }
 
         soloButton.setOnClickListener {
             val intent = Intent(this, SoloActivity::class.java)
